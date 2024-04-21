182f61c75e3415f92af653638699a00e823a025a
haldanejob <haldanejob@gmail.com>
2024-04-21 15:02:00 +0900

    first commit to prepare for deploying

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"simplebank/api"
+	"simplebank/util"
+
+	db "simplebank/db/sqlc"
+
+	_ "github.com/golang/mock/mockgen/model"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannnot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server, err := api.NewServer(config, *store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
